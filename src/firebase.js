0db34e4d15b44027678dae1a60192933377aaddc
Naresh K <nare95143@gmail.com>
2024-12-16 16:52:09 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set, onValue } from "firebase/database";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyDtmuVRYz8cUXgmZbTAfx04jZ2cW-dzgcY",
-	authDomain: "skragulmahal-add0f.firebaseapp.com",
-	databaseURL: "https://skragulmahal-add0f-default-rtdb.firebaseio.com",
-	projectId: "skragulmahal-add0f",
-	storageBucket: "skragulmahal-add0f.firebasestorage.app",
-	messagingSenderId: "141640710562",
-	appId: "1:141640710562:web:dd1c75272fca2c5cf29afb",
-	measurementId: "G-TYDPSY4CCJ"
+	apiKey: "YOUR-API-KEY",
+	authDomain: "YOUR-AUTH-DOMAIN",
+	databaseURL: "YOUR-DATABASE-URL",
+	projectId: "YOUR-PROJETC-ID",
+	storageBucket: "YOUR-STORAGE-BUCKET",
+	messagingSenderId: "MESSAGE-SENDER-ID",
+	appId: "APP-ID",
+	measurementId: "MEASUREMENT-ID"
   };
 
 const app = initializeApp(firebaseConfig);
